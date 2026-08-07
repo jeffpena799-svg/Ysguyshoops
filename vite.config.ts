211c2dcc07b6e991c13b2a785f3fec68b4f1548e
jeffpena799-svg <jeffpena799@gmail.com>
2026-08-06 21:02:55 -0500

    Enable Version 6.6 Home Redesign Preview

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { version632Feature } from "./version632.plugin";
 import { version64Feature } from "./version64.plugin";
 import { version65Feature } from "./version65.plugin";
+import { version66Feature } from "./version66.plugin";
 
 function replaceRequired(source: string, search: string, replacement: string): string {
   if (!source.includes(search)) {
@@ -44,4 +45,4 @@ function closeSundayFeature(): Plugin {
   };
 }
 
-export default defineConfig({plugins:[closeSundayFeature(),version632Feature(),version64Feature(),version65Feature(),react()]});
+export default defineConfig({plugins:[closeSundayFeature(),version632Feature(),version64Feature(),version65Feature(),version66Feature(),react()]});
