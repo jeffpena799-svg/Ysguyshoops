b7b1c0a08c44eda8c85f05429d8c0c371fba774a
jeffpena799-svg <jeffpena799@gmail.com>
2026-08-06 22:34:31 -0500

    Enable Version 6.7.1 My Player readability update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { version66Feature } from "./version66b.plugin";
 import { version66NavigationFix } from "./version66navfix.plugin";
 import { version66HomeComponent } from "./version66homecomponent.plugin";
 import { version67MyPlayer } from "./version67myplayer.plugin";
+import { version671Readability } from "./version671readability.plugin";
 
 function replaceRequired(source: string, search: string, replacement: string): string {
   if (!source.includes(search)) {
@@ -48,4 +49,4 @@ function closeSundayFeature(): Plugin {
   };
 }
 
-export default defineConfig({plugins:[closeSundayFeature(),version632Feature(),version64Feature(),version65Feature(),version66Feature(),version66NavigationFix(),version66HomeComponent(),version67MyPlayer(),react()]});
+export default defineConfig({plugins:[closeSundayFeature(),version632Feature(),version64Feature(),version65Feature(),version66Feature(),version66NavigationFix(),version66HomeComponent(),version67MyPlayer(),version671Readability(),react()]});
