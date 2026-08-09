a59a50466ea1178cc98c6d78c35beec04f1657b7
jeffpena799-svg <jeffpena799@gmail.com>
2026-08-08 23:52:41 -0500

    Enable My Player typography and edit strip polish

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ import { version684CommissionerFab } from "./version684commissionerfab.plugin";
 import { version685HomeCompact } from "./version685homecompact.plugin";
 import { version686RemovePlayerTimeline } from "./version686removeplayertimeline.plugin";
 import { version687MyPlayerCompact } from "./version687myplayercompact.plugin";
+import { version688MyPlayerPolish } from "./version688myplayerpolish.plugin";
 
 function replaceRequired(source: string, search: string, replacement: string): string {
   if (!source.includes(search)) {
@@ -58,4 +59,4 @@ function closeSundayFeature(): Plugin {
   };
 }
 
-export default defineConfig({plugins:[closeSundayFeature(),version632Feature(),version64Feature(),version65Feature(),version66Feature(),version66NavigationFix(),version66HomeComponent(),version67MyPlayer(),version671Readability(),version673HomePolish(),version68AroundLeague(),version681HallReadability(),version682HomeCleanup(),version682MyPlayerEditors(),version684CommissionerFab(),version685HomeCompact(),version686RemovePlayerTimeline(),version687MyPlayerCompact(),react()]});
+export default defineConfig({plugins:[closeSundayFeature(),version632Feature(),version64Feature(),version65Feature(),version66Feature(),version66NavigationFix(),version66HomeComponent(),version67MyPlayer(),version671Readability(),version673HomePolish(),version68AroundLeague(),version681HallReadability(),version682HomeCleanup(),version682MyPlayerEditors(),version684CommissionerFab(),version685HomeCompact(),version686RemovePlayerTimeline(),version687MyPlayerCompact(),version688MyPlayerPolish(),react()]});
