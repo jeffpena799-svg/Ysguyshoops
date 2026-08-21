7f81418d26f5ffcda374c65da18e849877f1a109
jeffpena799-svg <jeffpena799@gmail.com>
2026-08-21 13:26:54 -0500

    Enable Version 7.1 combined defense update

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ import { version7Foundation } from "./version7.plugin";
 import { version7Components } from "./version7components.plugin";
 import { version7ViewPrepare, version7ViewRestore } from "./version7viewbridge.plugin";
 import { version7LayoutFix } from "./version7layoutfix.plugin";
+import { version71CombinedDefense } from "./version71.plugin";
 
 function replaceRequired(source: string, search: string, replacement: string): string {
   if (!source.includes(search)) throw new Error(`Close Sunday patch could not find expected source: ${search.slice(0, 90)}`);
@@ -50,4 +51,4 @@ function closeSundayFeature(): Plugin {
   }};
 }
 
-export default defineConfig({plugins:[closeSundayFeature(),version632Feature(),version64Feature(),version65Feature(),version66Feature(),version66NavigationFix(),version66HomeComponent(),version67MyPlayer(),version671Readability(),version673HomePolish(),version68AroundLeague(),version681HallReadability(),version682HomeCleanup(),version682MyPlayerEditors(),version684CommissionerFab(),version685HomeCompact(),version686RemovePlayerTimeline(),version687MyPlayerCompact(),version688MyPlayerPolish(),version69HallHistory(),version691RecordRules(),version692WeeklyMvp(),version7ViewPrepare(),version7Foundation(),version7ViewRestore(),version7Components(),version7LayoutFix(),react()]});
+export default defineConfig({plugins:[closeSundayFeature(),version632Feature(),version64Feature(),version65Feature(),version66Feature(),version66NavigationFix(),version66HomeComponent(),version67MyPlayer(),version671Readability(),version673HomePolish(),version68AroundLeague(),version681HallReadability(),version682HomeCleanup(),version682MyPlayerEditors(),version684CommissionerFab(),version685HomeCompact(),version686RemovePlayerTimeline(),version687MyPlayerCompact(),version688MyPlayerPolish(),version69HallHistory(),version691RecordRules(),version692WeeklyMvp(),version7ViewPrepare(),version7Foundation(),version7ViewRestore(),version7Components(),version7LayoutFix(),version71CombinedDefense(),react()]});
