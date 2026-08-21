d03f1b9e2f33699ee3a27ce78fd92d8424f51ec3
jeffpena799-svg <jeffpena799@gmail.com>
2026-08-21 14:55:38 -0500

    Enable stable My Player renderer hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -26,10 +26,11 @@ import { version7Components } from "./version7components.plugin";
 import { version7ViewPrepare, version7ViewRestore } from "./version7viewbridge.plugin";
 import { version7LayoutFix } from "./version7layoutfix.plugin";
 import { version71CombinedDefense } from "./version71.plugin";
+import { version711MyPlayerFix } from "./version711myplayerfix.plugin";
 
 function replaceRequired(source: string, search: string, replacement: string): string {
   if (!source.includes(search)) throw new Error(`Close Sunday patch could not find expected source: ${search.slice(0, 90)}`);
-  return source.replace(search, replacement);
+  return source.replace(search,replacement);
 }
 function closeSundayFeature(): Plugin {
   return {name:"ys-guys-close-sunday",enforce:"pre",transform(source,id){
@@ -51,4 +52,4 @@ function closeSundayFeature(): Plugin {
   }};
 }
 
-export default defineConfig({plugins:[closeSundayFeature(),version632Feature(),version64Feature(),version65Feature(),version66Feature(),version66NavigationFix(),version66HomeComponent(),version67MyPlayer(),version671Readability(),version673HomePolish(),version68AroundLeague(),version681HallReadability(),version682HomeCleanup(),version682MyPlayerEditors(),version684CommissionerFab(),version685HomeCompact(),version686RemovePlayerTimeline(),version687MyPlayerCompact(),version688MyPlayerPolish(),version69HallHistory(),version691RecordRules(),version692WeeklyMvp(),version7ViewPrepare(),version7Foundation(),version7ViewRestore(),version7Components(),version7LayoutFix(),version71CombinedDefense(),react()]});
+export default defineConfig({plugins:[closeSundayFeature(),version632Feature(),version64Feature(),version65Feature(),version66Feature(),version66NavigationFix(),version66HomeComponent(),version67MyPlayer(),version671Readability(),version673HomePolish(),version68AroundLeague(),version681HallReadability(),version682HomeCleanup(),version682MyPlayerEditors(),version684CommissionerFab(),version685HomeCompact(),version686RemovePlayerTimeline(),version687MyPlayerCompact(),version688MyPlayerPolish(),version69HallHistory(),version691RecordRules(),version692WeeklyMvp(),version7ViewPrepare(),version7Foundation(),version7ViewRestore(),version7Components(),version7LayoutFix(),version71CombinedDefense(),version711MyPlayerFix(),react()]});
