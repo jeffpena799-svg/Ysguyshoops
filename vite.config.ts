3e18ef2f12263f30fcf2cc308888b919c477b115
jeffpena799-svg <jeffpena799@gmail.com>
2026-08-21 09:36:27 -0500

    Enable Version 7 foundation

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ import { version688MyPlayerPolish } from "./version688myplayerpolish.plugin";
 import { version69HallHistory } from "./version69hallhistory.plugin";
 import { version691RecordRules } from "./version691recordrules.plugin";
 import { version692WeeklyMvp } from "./version692weeklymvp.plugin";
+import { version7Foundation } from "./version7.plugin";
 
 function replaceRequired(source: string, search: string, replacement: string): string {
   if (!source.includes(search)) throw new Error(`Close Sunday patch could not find expected source: ${search.slice(0, 90)}`);
@@ -46,4 +47,4 @@ function closeSundayFeature(): Plugin {
   }};
 }
 
-export default defineConfig({plugins:[closeSundayFeature(),version632Feature(),version64Feature(),version65Feature(),version66Feature(),version66NavigationFix(),version66HomeComponent(),version67MyPlayer(),version671Readability(),version673HomePolish(),version68AroundLeague(),version681HallReadability(),version682HomeCleanup(),version682MyPlayerEditors(),version684CommissionerFab(),version685HomeCompact(),version686RemovePlayerTimeline(),version687MyPlayerCompact(),version688MyPlayerPolish(),version69HallHistory(),version691RecordRules(),version692WeeklyMvp(),react()]});
+export default defineConfig({plugins:[closeSundayFeature(),version632Feature(),version64Feature(),version65Feature(),version66Feature(),version66NavigationFix(),version66HomeComponent(),version67MyPlayer(),version671Readability(),version673HomePolish(),version68AroundLeague(),version681HallReadability(),version682HomeCleanup(),version682MyPlayerEditors(),version684CommissionerFab(),version685HomeCompact(),version686RemovePlayerTimeline(),version687MyPlayerCompact(),version688MyPlayerPolish(),version69HallHistory(),version691RecordRules(),version692WeeklyMvp(),version7Foundation(),react()]});
